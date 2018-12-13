b615a1f91838ad5bf648cafdef4482d578da871b
AldoOct <andnert@gmail.com>
2018-12-13 18:55:44 +0700

    Nyoba Laravel CRUD

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -13,7 +13,7 @@
     <!-- Canonical -->
     <link rel="canonical" href="">
 
-    <title>Anak Setan</title>
+    <title>User</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
